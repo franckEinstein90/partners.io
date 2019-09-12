b8ea90ce843b8a789f64bc42a9a1635e14c31efc
U-EMERGEDESK\Franck <franckbinard@gmail.com>
2019-09-12 18:58:10 -0400

    finally added a root app module

---
+++ src/pardners.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+const pardners = (function(){
+    let _version
+
+    return {
+        getVersion: function(){
+            //returns current version of app
+
+        }
+
+    }
+
+}()
+
+
+module.exports = {
+    pardners
+}
